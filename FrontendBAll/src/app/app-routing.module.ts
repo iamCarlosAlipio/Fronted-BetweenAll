e5a42894805cebf06bc3c269e07245de97dde5bd
Cesar Matos <102164937+jcesarmatoss@users.noreply.github.com>
2023-05-02 01:23:59 -0500

    Merge develop into feature/Julio_Matos

--- FrontendBAll/src/app/app-routing.module.ts
+++ FrontendBAll/src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
-=======
 import { AddSocialEventComponent } from './components/add-social-event/add-social-event.component';
 import { GroupsComponent } from './components/groups/groups.component';
 import { AddEditGroupsComponent } from './components/add-edit-groups/add-edit-groups.component';
->>>>>>> 29f1b8097683491b71784e1a9eebe964ca906cb2
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
