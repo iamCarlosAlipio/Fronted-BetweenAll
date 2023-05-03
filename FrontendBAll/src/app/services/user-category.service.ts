d53eaf322c465b950630545012ad5e8ecbd7ed02
Carlos Sanchez <u20201c222@upc.edu.pe>
2023-05-02 23:04:59 -0500

    Login final part2

--- FrontendBAll/src/app/services/user-category.service.ts
+++ FrontendBAll/src/app/services/user-category.service.ts
@@ -1,9 +1,15 @@
+import { HttpBackend } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { userCategory } from '../models/userCategory';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserCategoryService {
 
-  constructor() { }
+  RutaServidor : string = "http://localhost:3000";
+  Recurso: string ="userCategory";
+  
+  constructor(private http:HttpBackend) { }
+
 }
